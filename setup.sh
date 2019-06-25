9461232b6204fdcb4f58850f2306956a0d342a9a
infragilis <infragilis@users.noreply.github.com>
2019-06-25 15:06:12 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# this will get the container updated with python3, and the pstools mod moved to the correct location
+# this will get the container/ansible updated with python3, and the pstools mod moved to the correct location
 yum -y install python36-setuptools
 easy_install-3.6 pip
 pip3 install ansible
