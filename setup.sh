9135bc71c0f24a8e9632f4eb083c2de2a3cd44a6
root <sboonstra@gmail.com>
2019-06-25 21:18:56 +0000

    copy

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 yum -y install python36-setuptools
 easy_install-3.6 pip
 pip3 install ansible
-mv ansible/lib/ansible/* /usr/local/lib/python3.6/site-packages/ansible
+cp -r /hci/ansible/lib/ansible/module_utils/pstools /usr/local/lib/python3.6/site-packages/ansible/module_utils/
+cp -r /hci/ansible/lib/ansible/modules/pstools/ /usr/local/lib/python3.6/site-packages/ansible/modules/
 #mv ansible /tmp/
-cp /usr/local/lib/python3.6/site-packages/ansible/modules/pstools/testping.yml /hci
+
