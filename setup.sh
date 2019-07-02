2db0bb17c838cf7a7c7cbc5ea1ba6c5149be9a25
infragilis <infragilis@users.noreply.github.com>
2019-07-02 15:00:59 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # this will get the container/ansible updated with python3, and the pstools mod moved to the correct location
 yum -y install python36-setuptools
+yum -y install iptables
 easy_install-3.6 pip
 pip3 install ansible
 cp -r /hci/ansible/lib/ansible/module_utils/pstools /usr/local/lib/python3.6/site-packages/ansible/module_utils/
