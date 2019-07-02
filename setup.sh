2da28d4963f4693ea66aa2aed9e24097086050c6
infragilis <infragilis@users.noreply.github.com>
2019-07-02 15:12:43 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # this will get the container/ansible updated with python3, and the pstools mod moved to the correct location
 yum -y install python36-setuptools
-yum -y install iptables
+yum -y install iptables-services
 easy_install-3.6 pip
 pip3 install ansible
 cp -r /hci/ansible/lib/ansible/module_utils/pstools /usr/local/lib/python3.6/site-packages/ansible/module_utils/
@@ -9,10 +9,11 @@ cp -r /hci/ansible/lib/ansible/modules/pstools/ /usr/local/lib/python3.6/site-pa
 pip install netapp-lib
 pip install solidfire-sdk-python
 pip3 install xlsxwriter
-chkconfig iptables on
-iptables -A INPUT -p udp --dport 2020 -j ACCEPT
-iptables -A OUTPUT -p udp --dport 2020 -j ACCEPT
+systemctl enable iptables
+/sbin/iptables -A INPUT -p udp --dport 2020 -j ACCEPT
+/sbin/iptables -A OUTPUT -p udp --dport 2020 -j ACCEPT
 service iptables save
+
 
 touch /usr/local/lib/python3.6/site-packages/ansible/module_utils/pstools/__init__.py
 cp /usr/local/lib/python3.6/site-packages/ansible/modules/pstools/testping.yml /hci/
