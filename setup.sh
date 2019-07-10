3f0d13c79cb994b0a7e643b043ecdb814d7cfdb4
infragilis <infragilis@users.noreply.github.com>
2019-07-10 14:00:39 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 # this will get the container/ansible updated with python3, and the pstools mod moved to the correct location
-yum -y install python36-setuptools
-easy_install-3.6 pip
-pip3 install ansible
+mkdir /usr/local/lib/python3.6/site-packages/
+mkdir /usr/local/lib/python3.6/site-packages/ansible/
+mkdir /usr/local/lib/python3.6/site-packages/ansible/module_utils/
+mkdir /usr/local/lib/python3.6/site-packages/ansible/modules/
 cp -r /hci/ansible/lib/ansible/module_utils/pstools /usr/local/lib/python3.6/site-packages/ansible/module_utils/
 cp -r /hci/ansible/lib/ansible/modules/pstools/ /usr/local/lib/python3.6/site-packages/ansible/modules/
-pip install netapp-lib
-pip install solidfire-sdk-python
-pip3 install xlsxwriter
-
 
 
 touch /usr/local/lib/python3.6/site-packages/ansible/module_utils/pstools/__init__.py
